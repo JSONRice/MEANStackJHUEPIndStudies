f1492e1750df12a9f091d50eaa10212a1761d2cc
User <User@10.0.2.15>
2016-02-02 16:09:10 -0500

    Resolved bower package dependency. Bower WILL install and run regardless of the OS be it Windows 7, Mac OS X, or Linux CentOS which is really RHEL.

--- server.js
+++ server.js
@@ -41,15 +41,13 @@ app.get('/api/todos', function(req, res) {
 
 		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 		if (err)
-		    res.send(err)
-
-			res.json(todos); // return all todos in JSON format
+		    res.send(err);
+                    res.json(todos); // return all todos in JSON format
 	    });
     });
 
 // create todo and send back all todos after creation
 app.post('/api/todos', function(req, res) {
-
         // create a todo, information comes from AJAX request from Angular
         Todo.create({
 		text : req.body.text,
@@ -60,12 +58,12 @@ app.post('/api/todos', function(req, res) {
 
 		// get and return all the todos after you create another
 		Todo.find(function(err, todos) {
-			if (err)
-			    res.send(err)
-				res.json(todos);
+			if (err) {
+			    res.send(err);
+                        }
+			res.json(todos);
 		    });
 	    });
-
     });
 
 // delete a todo
@@ -73,14 +71,16 @@ app.delete('/api/todos/:todo_id', function(req, res) {
 	Todo.remove({
 		_id : req.params.todo_id
 		    }, function(err, todo) {
-		if (err)
+		if (err) {
 		    res.send(err);
+                }
 
 		// get and return all the todos after you create another
 		Todo.find(function(err, todos) {
-			if (err)
-			    res.send(err)
-				res.json(todos);
+			if (err) {
+			    res.send(err);
+                        }
+			res.json(todos);
 		    });
 	    });
     });
@@ -91,7 +91,7 @@ app.delete('/api/todos/:todo_id', function(req, res) {
 	//        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	//    });
 
-var port = 8081;
+var port = 8080;
 
 fs.readFile('./public/templates/index.html', function (err, html) {
 	if (err) {
