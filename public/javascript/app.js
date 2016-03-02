12edb749377d827620158dd8346937aae2f4a6db
jsnrice <jsnrice@gmail.com>
2016-03-01 20:38:01 -0500

    Tree controller with MEAN data set. Tree is just a Twitter Bootstrap navigation bar.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -8,16 +8,23 @@ var meanstacktutorials = angular.module('meanstacktutorials', [
   'ui.select', 'ui.keypress', 'nvd3', 'ngLodash', 'angularSpinner'
 ]);
 
+// Custom filter to take string and trust as html
+meanstacktutorials.filter('htmlify', function ($sce) {
+  return function (val) {
+    return $sce.trustAsHtml(val);
+  };
+});
+
 // calls the services to force pre-load
 // this is good for calling services (REST data fetches) prior to page loads
 /*
-meanstacktutorials.run([
-  'homeService',
-  function (homeService) {
-    // intentionally left blank
-  }
-]);
-*/
+ meanstacktutorials.run([
+ 'homeService',
+ function (homeService) {
+ // intentionally left blank
+ }
+ ]);
+ */
 
 // The following ensures that a user is logged in prior to any view being loaded.
 // Restricted pages get re-routed back to the login view.
@@ -29,12 +36,9 @@ meanstacktutorials.run(function ($rootScope, $location, $route, AuthenticationSe
   });
 });
 
-
-console.log('dependencies pre-loaded in public/javascript/app.js');
-
 meanstacktutorials.config(['$routeProvider', function ($routeProvider) {
     console.log('meanstacktutorials.config > routing');
-    
+
     // note: chain on additional route configs with .when(...)
     // The otherwise() (default) page loads from '/' and is the login page
     // All other pages should be restricted access besides the registration,
@@ -42,31 +46,32 @@ meanstacktutorials.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
       templateUrl: '../templates/login.html',
       controller: 'LoginController',
-      access: {restricted: false}      
+      access: {restricted: false}
     }).when('/login', {
-      templateUrl: '../templates/login.html', 
+      templateUrl: '../templates/login.html',
       controller: 'LoginController',
-      access: {restricted: false}      
+      access: {restricted: false}
     }).when('/logout', {
       controller: 'LogoutController'
     }).when('/register', {
-      templateUrl: '../templates/register.html', 
+      templateUrl: '../templates/register.html',
       controller: 'RegisterController',
-      access: {restricted: false}      
+      access: {restricted: false}
     }).when('/treeTest', {
-      templateUrl: '../templates/treeTest.html', 
+      templateUrl: '../templates/treeTest.html',
       controller: 'TreeController',
-      access: {restricted: false}      
+      access: {restricted: false}
     })
-      // NOTICE: The following route controller configurations are all restricted access
-      // If you add a new config then make sure it's access is restricted.
-      .when('/home', {
-      templateUrl: '../templates/home.html',
-      controller: 'HomeController',
-      access: {restricted: true}      
-    })    
-      .otherwise({
-      redirectTo: '/login'
-    });
+            // NOTICE: The following route controller configurations are all restricted access
+            // If you add a new config then make sure it's access is restricted.
+            .when('/home', {
+              templateUrl: '../templates/home.html',
+              controller: 'HomeController',
+              access: {restricted: true}
+            })
+            .otherwise({
+              redirectTo: '/login'
+            });
   }]);
 
+console.log('dependencies pre-loaded in public/javascript/app.js');
--- public/javascript/controllers/treeController.js
+++ public/javascript/controllers/treeController.js
@@ -7,44 +7,51 @@ angular.module('meanstacktutorials').controller('TreeController', [
   function ($scope, $timeout) {
     var apple_selected;
     var tree;
+    var treedata_mean;
     var treedata_avm;
     var treedata_geography;
-    
+
     $scope.my_tree_handler = function (branch) {
-      var _ref;
-      $scope.output = "You selected: " + branch.label;
-      if ((_ref === branch.data) !== null ? _ref.description : void 0) {
-        return $scope.output += '(' + branch.data.description + ')';
+      $scope.output = "<h1>" + branch.label + "</h1>";
+      if (!branch.data) {
+        return $scope.output;
+      } else {
+        return $scope.output += '<br/>' + branch.data.description;
       }
     };
     apple_selected = function (branch) {
-      return $scope.output = "APPLE! : " + branch.label;
+      return $scope.output = "An Apple was selected! : " + branch.label;
     };
-    treedata_avm = [
-      {
-        label: 'Animal',
+    treedata_mean = [{
+        label: 'MEAN Stack Introduction',
+        data: {
+          description: "Welcome to the MEAN Stack tutorials! MEAN stands for <b>MongoDB</b>, <b>ExpressJS</b>, <b>AngularJS</b>, and <b>NodeJS</b> Click on one of the links below within the Table of Contents on the left to acquire more info about a topic, see live demos, and play around with the code."
+        },
         children: [
           {
-            label: 'Dog',
+            label: 'MongoDB',
             data: {
-              description: "man's best friend"
+              description: "MongoDB (from humongous) is a cross-platform document-oriented database. Classified as a NoSQL database, MongoDB eschews the traditional table-based relational database structure in favor of JSON-like documents with dynamic schemas (MongoDB calls the format BSON), making the integration of data in certain types of applications easier and faster. MongoDB is developed by MongoDB Inc. and is published as free and open-source software under a combination of the GNU Affero General Public License and the Apache License. As of July 2015, MongoDB is the fourth most popular type of database management system, and the most popular for document stores."
             }
           }, {
-            label: 'Cat',
+            label: 'ExpressJS',
             data: {
-              description: "Felis catus"
+              description: "Express.js is a Node.js web application server framework, designed for building single-page, multi-page, and hybrid web applications. It is the de facto standard server framework for node.js. The original author, TJ Holowaychuk, described it as a Sinatra-inspired server, meaning that it is relatively minimal with many features available as plugins. Express is the backend part of the MEAN stack, together with MongoDB database and AngularJS frontend framework. In June 2014, rights to manage the project were acquired by StrongLoop. StrongLoop was acquired by IBM in September 2015; [5] in January 2016, IBM announced that it would place Express.js under the stewardship of the Node.js Foundation incubator."
             }
           }, {
-            label: 'Hippopotamus',
+            label: 'AngularJS',
             data: {
-              description: "hungry, hungry"
+              description: "AngularJS (commonly referred to as \"Angular\" or \"Angular.js\") is an open-source web application framework mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications. It aims to simplify both the development and the testing of such applications by providing a framework for client-side model–view–controller (MVC) and model–view–viewmodel (MVVM) architectures, along with components commonly used in rich Internet applications."
             }
           }, {
-            label: 'Chicken',
-            children: ['White Leghorn', 'Rhode Island Red', 'Jersey Giant']
+            label: 'NodeJS',
+            data: {
+              description: "Node.js is an open-source, cross-platform runtime environment for developing server-side Web applications. Although Node.js is not a JavaScript framework,[3] many of its basic modules are written in JavaScript, and developers can write new modules in JavaScript. The runtime environment interprets JavaScript using Google's V8 JavaScript engine."
+            }
           }
         ]
-      }, {
+      }];
+    treedata_avm = [{
         label: 'Vegetable',
         data: {
           definition: "A plant or part of a plant used as food, typically as accompaniment to meat or fish, such as a cabbage, potato, carrot, or bean.",
@@ -127,7 +134,9 @@ angular.module('meanstacktutorials').controller('TreeController', [
         ]
       }
     ];
-    $scope.my_data = treedata_avm;
+    
+    // The other tree datasets are mainly for test purposes and examples. Migrate these to a test controller in Jasmine (Karma).
+    $scope.my_data = treedata_mean;
     $scope.try_changing_the_tree_data = function () {
       if ($scope.my_data === treedata_avm) {
         return $scope.my_data = treedata_geography;
